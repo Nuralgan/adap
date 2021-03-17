7accf2ec1fe98e186fb65abfd8b37cdd6690efed
Nuralgan <sisikeydj1221@gmail.com>
2021-03-17 15:44:53 +0600

    adap

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#video-b').click(function(){
+        let about = $('.modal')
+
+        if(about.css('display') === 'none'){
+            about.css('display','block')
+        } else {
+            about.css('display','none')
+        }
+    })
+
+    $('.modal-close').click(function(){
+        $('.modal').css('display','none')
+    })
+})
